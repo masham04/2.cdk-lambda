7013b4694f3591a419e20bc8aa459a3f5f99cdc0
Muhammad Masham <66207787+masham04@users.noreply.github.com>
2021-04-25 14:58:33 +0500

    new

--- lib/hello-lambda-stack.ts
+++ lib/hello-lambda-stack.ts
@@ -1,9 +1,15 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class HelloLambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const helloLambda = new lambda.Function(this, 'hellolambda', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: 'hello.handler',
+      code: lambda.Code.fromAsset('lambda'),
+    });
   }
 }
